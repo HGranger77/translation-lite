603543a96614e01a0bd6b67b4dc2505578127f7f
HGranger77 <harrisonhgranger@gmail.com>
2025-08-28 01:27:04 +1000

    Fixed lang codes

--- src/server/model.py
+++ src/server/model.py
@@ -9,23 +9,21 @@ class Model:
         self.model = AutoModelForSeq2SeqLM.from_pretrained(model_path).to(self.device)
 
     def get_lang_code(self, language):
-        language_map = {
-            "French": "fr",
-            "German": "de",
-            "Spanish": "es",
-            "Italian": "it",
-            "Chinese": "zh"
+        lang_code_map = {
+            "French": "fra_Latn",
+            "German": "deu_Latn",
+            "Spanish": "spa_Latn",
+            "Italian": "ita_Latn",
+            "Chinese": "zho_Hans"
         }
-        lang_code = language_map.get(language)
+        lang_code = lang_code_map.get(language)
         if not lang_code:
             raise NotImplementedError(f"Unsupported language: {language}")
-        lid_token = f"<{lang_code}>"
-        lid_id = self.tokenizer.convert_tokens_to_ids(lid_token)
-        return lid_id
+        return lang_code
 
     def infer(self, text, language):
         language_code = self.get_lang_code(language)
         inputs = self.tokenizer(text, return_tensors="pt").to(self.device)
-        generated_tokens = self.model.generate(**inputs, forced_bos_token_id=language_code)
+        generated_tokens = self.model.generate(**inputs, forced_bos_token_id=self.tokenizer.convert_tokens_to_ids(language_code))
         translated_text = self.tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
         return translated_text
